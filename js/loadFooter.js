2d974cd55045390d1ee8522681d7aec944c6174f
roxana lazarte <WJ918xd@hotmail.com>
2025-03-28 15:28:31 -0500

    Pagina personalizada Upminaa para redes 2

---
+++ js/loadFooter.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => document.getElementById("footer").innerHTML = data)
+        .catch(error => console.error("Error al cargar el footer:", error));
+});
