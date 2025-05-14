984867cfea1ddae0842803e4a1eea7769bf28e93
Hack-Gamer001 <119648000+Hack-Gamer001@users.noreply.github.com>
2025-05-14 06:26:55 +0000

    Minaa update

--- js/loadFooter.js
+++ js/loadFooter.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("footer.html")
+    fetch("../footer.html")  // Cambia la ruta para apuntar al directorio raíz
         .then(response => response.text())
         .then(data => document.getElementById("footer").innerHTML = data)
         .catch(error => console.error("Error al cargar el footer:", error));
